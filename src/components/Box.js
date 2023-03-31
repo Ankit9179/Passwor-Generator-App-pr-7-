f7dee0c52de4ceeb376293f05a1bdaa3b5c4ef94
Ankit9179 <rahiankit10189@gmail.com>
2023-03-31 13:01:00 +0530

    add a feature (enter number for password lenfth)

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,14 +5,19 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Box = () => {
     const [state,setState] = useState('');
+    const [lett,setLett] = useState('')
 
     const generatorPassword = () =>{
+        if(lett===''){
+            alert('Enter number for password length ')
+        }else{
         let pass = "" ;
         let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
-        for(let i=1; i<=8; i++){
+        for(let i=1; i<=lett; i++){
             let charr = Math.floor(Math.random() * string.length);
             pass += string.charAt(charr)
             setState(pass)
+        }
         }
     }
 
@@ -25,11 +30,15 @@ const Box = () => {
         }else{toast("Your Password Copied")}
     }
 
+    const lettfunc = (e) =>{
+        setLett(e.target.value)
+    }
     
 
     return <>
         <div className="container">
             <h2>Randome Password Generator </h2>
+            <input type="text" placeholder="Enter number for password length" onChange={lettfunc} />
             <input type="text" readOnly disabled name="" placeholder="Password" value={state} id="" />
             <div className="btns">
                 <button onClick={generatorPassword}>Generat Password</button>
